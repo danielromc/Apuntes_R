2ab1b8072dde2753b0dd08d0e7204e634b05a1b3
Daniel Román <89816628+danielromc@users.noreply.github.com>
2023-05-26 13:22:17 +0200

    Add files via upload

---
+++ introduccion-a-R/convertir-numero-a-string.R
@@ -0,0 +1,10 @@
+#Crear una función que convierta un número entero en cadena
+
+#Se define la función personalizada con el parámetro n y se asigna a una variable
+number_to_string <- function(n) {
+
+#Se aplica la funcion as.character() que trasnforma elementos en string
+  cadena <- as.character(n)
+  print(cadena)
+}
+
