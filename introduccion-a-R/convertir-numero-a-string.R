1395a752e6d1dfa06b5ee8efcbf37d0397f63828
Daniel Román <89816628+danielromc@users.noreply.github.com>
2023-05-26 13:24:40 +0200

    convertir-numero-a-string.R

--- introduccion-a-R/convertir-numero-a-string.R
+++ introduccion-a-R/convertir-numero-a-string.R
@@ -3,8 +3,7 @@
 #Se define la función personalizada con el parámetro n y se asigna a una variable
 number_to_string <- function(n) {
 
-#Se aplica la funcion as.character() que trasnforma elementos en string
+#Se aplica la funcion as.character() que transforma elementos en string
   cadena <- as.character(n)
   print(cadena)
 }
-
